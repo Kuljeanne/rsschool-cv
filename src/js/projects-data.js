38d3fdf447143d5ac8a11ddb8e9522a2ed7b6890
Zhanna Kulaginae <kuljeanne@gmail.com>
2023-07-20 15:25:21 +0300

    fix: add adaptive, changw styles, add info

---
+++ src/js/projects-data.js
@@ -0,0 +1,48 @@
+window.data = [
+  {
+    title: "Plants",
+    link: "https://rolling-scopes-school.github.io/kuljeanne-JSFEPRESCHOOL2022Q4/plants/",
+    imgSrc: "src/img/plants.jpg",
+    description: "Адаптивный сайт интернет магазина",
+  },
+  {
+    title: "CSSBayan",
+    link: "https://kuljeanne.github.io/cssBayan",
+    imgSrc: "src/img/css-bayan.jpg",
+    description: "Аккордеон реализован только на css, без использования js. Дизайн адаптивный.",
+  },
+  {
+    title: "Momentum",
+    link: "https://rolling-scopes-school.github.io/kuljeanne-JSFEPRESCHOOL2022Q4/momentum/dist/",
+    imgSrc: "src/img/momentum.jpg",
+    description:
+      "Аналог одноимённого приложения Chrome. Приложение показывает время и имя пользователя. Фоновое изображение и приветствие меняются в зависимости от времени суток. В приложении есть часы, слайдер изображений, виджеты погоды, аудиоплеер, блок цитата дня, настройки. Для хранения имени пользователя и населённого пункта используется локальное хранилище - local storage.",
+  },
+  {
+    title: "Card Game",
+    link: "https://kuljeanne.github.io/Card_game/dist/",
+    imgSrc: "src/img/card-game.jpg",
+    description:
+      "Карточная игра на память-поиск пар. Реализована возможность выбора сложности. при старте игры дается 5 секунд на запоминание карт, далее карты переворачиваются и начинается отсчет времени на игру.",
+  },
+  {
+    title: "Камень, ножницы, бумага",
+    link: "https://kuljeanne.netlify.app",
+    imgSrc: "src/img/rock-papper-scissors.jpg",
+    description: "Браузерная игра. Интерфейс предлагает залогиниться. После старта игры начинаем ожидать соперника. После нажатия на фигуру получаем ответ со статусом: «победа», «поражение» или «ожидание хода игрока». В случае победы/поражения отрисовываем соответствующий экран и даем возможность вернуться в лобби/начать новую игру.",
+  },
+  {
+    title: "KeyBoard",
+    link: "https://kuljeanne.github.io/virtual-keyboard/dist/",
+    imgSrc: "src/img/keyboard.png",
+    description:
+      "Виртуальная клавиатура: реализована смена языка, смена регистра. Для печати можно использовать мышку и клавиатуру",
+  },
+  {
+    title: "Cinematheque (React)",
+    link: "https://github.com/Kuljeanne/cinematheque",
+    imgSrc: "src/img/cinema.jpg",
+    description:
+      "Библиотека кино.  Реализована возможность регистрации через localStorage, удаление и добавление в избранное для авторизацированных пользователей, сохранение истории поиска.",
+  },
+];
